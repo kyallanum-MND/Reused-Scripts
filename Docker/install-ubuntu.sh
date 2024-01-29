ee2890fdf5ae1338d5873eb7f0cc20d4f6ea893c
Kyal Lanum <83832885+kyallanum-MND@users.noreply.github.com>
2024-01-29 10:43:00 -0700

    Create install-ubuntu.sh

---
+++ Docker/install-ubuntu.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Run this script with sudo to ensure that everything runs properly.
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run this script as root."
+  exit 1
+fi
+
+# Install pre-requisites to install docker with apt-get
+apt-get update && apt-get install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+echo "Adding repository to keyring"
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list
+echo "Added..."
+
+echo "Installing docker & docker compose plugin"
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+usermod -aG docker $USER
+
+echo "Log out and back in for changes to occur"
